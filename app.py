034d4d5546db5ff638f0f97e7a9fbd46592b378a
Vanessa Pérez <miroslava2708@gmail.com>
2025-04-08 15:40:53 -0600

    Primer filtro de Fabricantes con menos de 1000 anuncios

--- app.py
+++ app.py
@@ -11,4 +11,19 @@ data_clean = pd.read_csv("C:/Users/Vane/Desktop/PracticasTripletenVane/06DataCar
 # Subtítulo
 st.subheader("Data Viewer")
 
-st.write(data_clean.head())
+  
+# Contar la cantidad de anuncios por fabricante
+ads_by_manufacturer = data_clean['manufacturer'].value_counts()
+
+# Checkbox para incluir fabricantes con menos de 1000 anuncios
+include_less_than_1000 = st.checkbox("Incluir fabricantes con menos de 1000 anuncios")
+
+# Filtrar fabricantes si el checkbox está activado
+if include_less_than_1000:
+    manufacturers_filtered = ads_by_manufacturer[ads_by_manufacturer < 1000].index
+    data_filtered = data_clean[data_clean['manufacturer'].isin(manufacturers_filtered)]
+else:
+    data_filtered = data_clean
+
+st.dataframe(data_filtered)
+
